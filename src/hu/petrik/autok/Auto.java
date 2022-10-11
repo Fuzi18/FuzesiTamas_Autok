0c657fe3281cd950c34a0b211eee1f2df2ba4bd7
Fuzi18 <fuzesi.tamas@petrik.hu>
2022-10-11 16:17:46 +0200

    s

--- src/hu/petrik/autok/Auto.java
+++ src/hu/petrik/autok/Auto.java
@@ -1,4 +1,6 @@
 package hu.petrik.autok;
 
 public interface Auto {
+    void gyorsul();
+    double getSebesseg();
 }
--- src/hu/petrik/autok/Tesla.java
+++ src/hu/petrik/autok/Tesla.java
@@ -1,4 +1,18 @@
 package hu.petrik.autok;
 
 public class Tesla implements Auto{
+    private double sebesseg = 0;
+
+    Tesla() {
+        sebesseg = Math.floor(Math.random() * 60);
+    }
+    @Override
+    public void gyorsul() {
+        sebesseg += Math.floor(Math.random() * 10) + 5;
+    }
+
+    @Override
+    public double getSebesseg() {
+        return sebesseg;
+    }
 }
